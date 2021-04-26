022cd271c6756f7165430f277de2d9dbe070fff8
Yash Choudhari <yashchoudhari.ysc@gmail.com>
2021-04-26 22:01:34 +0530

    Improved layout design

--- lib/main.dart
+++ lib/main.dart
@@ -15,58 +15,74 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             // ignore: deprecated_member_use
             children: <Widget>[
               // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  soundPlayer(1);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    soundPlayer(1);
+                  },
+                ),
               ),
               // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  soundPlayer(2);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    soundPlayer(2);
+                  },
+                ),
               ),
               // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  soundPlayer(3);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    soundPlayer(3);
+                  },
+                ),
               ),
               // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  soundPlayer(4);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    soundPlayer(4);
+                  },
+                ),
               ),
               // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.cyan,
-                onPressed: () {
-                  soundPlayer(5);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.cyan,
+                  onPressed: () {
+                    soundPlayer(5);
+                  },
+                ),
               ),
               // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  soundPlayer(6);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    soundPlayer(6);
+                  },
+                ),
               ),
               // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.purpleAccent,
-                onPressed: () {
-                  soundPlayer(7);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.purpleAccent,
+                  onPressed: () {
+                    soundPlayer(7);
+                  },
+                ),
               ),
             ],
           ),
